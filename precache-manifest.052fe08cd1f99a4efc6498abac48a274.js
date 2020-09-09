894d63dd0309a23dad6e85db7f60b857d93a9fba
Sourav Kumar <sourav.kumar@outlook.com>
2020-09-09 11:35:39 +0530

    Updates

---
+++ precache-manifest.052fe08cd1f99a4efc6498abac48a274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ed5a5be11a222a95d35f019fc1c29f",
+    "url": "/GitHub-Compare/index.html"
+  },
+  {
+    "revision": "09e94450f8a51c9d3553",
+    "url": "/GitHub-Compare/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b260b273219da9a021ac",
+    "url": "/GitHub-Compare/static/js/2.b65c9669.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GitHub-Compare/static/js/2.b65c9669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e94450f8a51c9d3553",
+    "url": "/GitHub-Compare/static/js/main.3016a735.chunk.js"
+  },
+  {
+    "revision": "1db13cc106d7238a37d2",
+    "url": "/GitHub-Compare/static/js/runtime-main.f37d9388.js"
+  }
+]);
